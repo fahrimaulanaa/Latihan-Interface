d56904448f344466732abc17903876322e2369b7
Fahri Maulana <88439222+fahrimaulanaa@users.noreply.github.com>
2022-03-29 19:55:30 +0700

    commit

--- Interpes.java
+++ Interpes.java
@@ -2,3 +2,7 @@
 public interface Interpes {   
     public void method1(); //Nama method untuk interface nya
 }
+
+interface hitung{
+    public void hitung(); //Nama method untuk interface yang akan berisi rumus matematika
+}
