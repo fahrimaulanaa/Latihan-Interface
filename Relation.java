93d56602a1058f3fa24d646d1839b79d207c1a0c
Fahri Maulana <88439222+fahrimaulanaa@users.noreply.github.com>
2022-03-29 19:01:42 +0700

    Initial commit

--- Relation.java
+++ Relation.java
@@ -7,7 +7,7 @@ public class Relation {
         }
     }
     public static void main(String[] args) { 
-        interfes obj = new Relation().new interfes();
-        obj.method1();
+        interfes obj = new Relation().new interfes(); //Deklarasi objek baru bernama obj
+        obj.method1(); //Memanggil method1 yang ada di class interfes tetapi methodnya memiliki isi di class Relation
     }
 }
